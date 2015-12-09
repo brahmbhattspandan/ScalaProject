e0c9ce712288f9c8002f6fafd605ec3f5a5301c7
Spandan Brahmbhatt <brahmbhattspandan@gmail.com>
2015-12-08 23:46:22 -0500

    Project Updated

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# ScalaProject
+# Value at Risk Analysis
+
+[Presentation for this project](https://www.youtube.com/watch?v=zdmgxdJoouU)
+
+
+In order to run, start a Cluster (prefered EMR cluster).
+
+
+You need to have `Zeppelin` and `Spark installed`.
+
+* [Spark Installation instruction](http://spark.apache.org/docs/latest/building-spark.html)
+
+* [Zeppelin Installtion instruction](http://zeppelin-project.org/docs/install/install.html)
+
+
+Now install Git.
+```shell
+sudo yum install -y git
+sudo apt-get install git
+```
+
+
+Clone this repo.
+```shell
+git clone https://github.com/brahmbhattspandan/MonteCarloData.git
+```
+
+Copy the folder `2AXV35H5P` into your zeppeline directory
+
+Refer [Copy Zeppeline Notebook](http://fedulov.website/2015/10/16/export-apache-zeppelin-notebooks/)
+
+
+Now you need to inport the external jars.
+
+Refer [Import dependency in Zeppelin](http://fedulov.website/2015/10/16/export-apache-zeppelin-notebooks/)
+
+
+`Make sure that you download the dependency before running spark code`
+
+If you are using first method, then use following code in Zeppeline notebook.
+
+```scala
+%dep
+z.reset()
+z.load("com.github.nscala-time:nscala-time_2.9.1:1.4.0")
+z.load("org.apache.commons:commons-math3:3.5")
+```
+
+Open the notebook and `run all paragraph` and you will get thr VAR for all the stocks. If you need specific stocks related VAR then remove those files from `/home/hadoop/MonteCarloData/data/stocks/`
+
+You can look at some of the graph related to data here.
+[Bonds](https://public.tableau.com/profile/smit.shah#!/vizhome/Bonds/Story1)
+[Google](https://public.tableau.com/profile/smit.shah#!/vizhome/Stock-Google-WithFactors/Google)
--- SparkFA/src/main/scala-2.11/SimpleSpark.scala
+++ SparkFA/src/main/scala-2.11/SimpleSpark.scala
@@ -1,12 +1,6 @@
-/**
-  * Created by smit on 11/20/15.
-  */
-
 
 import java.io.File
 import java.text.SimpleDateFormat
-
-
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import java.util
@@ -20,7 +14,6 @@ import org.apache.spark.mllib.feature.{StandardScaler,Normalizer,ChiSqSelector}
 import org.apache.spark.mllib.evaluation.{MulticlassMetrics, BinaryClassificationMetrics}
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.SQLContext._
-//import sqlContext.implicits._
 import org.apache.spark.rdd.PairRDDFunctions
 import org.apache.spark.mllib.linalg.Matrix
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
@@ -41,11 +34,9 @@ import com.github.nscala_time.time.Imports._
 
 
 object SimpleSpark {
-
-  //--------------------------------------------------------------------------------------------
-  /**
-    * maps a Stock name to ID
-    */
+  /*
+   * maps a Stock name to ID
+   */
   def readStockNameMap(file: File) = {
     val lines = Source.fromFile(file).getLines().toSeq
     lines.tail.map(line => {
@@ -54,10 +45,9 @@ object SimpleSpark {
       (cols(1), value)
     }).toMap
   }
-
-  /**
-    * maps a ID to Stock name
-    */
+  /*
+   * maps a ID to Stock name
+   */
   def readIdtoStockNameMap(file: File) = {
     val lines = Source.fromFile(file).getLines().toSeq
     lines.tail.map(line => {
@@ -66,9 +56,7 @@ object SimpleSpark {
       (value, cols(1))
     }).toMap
   }
-
-  //--------------------------------------------------------------------------------------------
-  //Re useable Scala methods
+ 
   def toInt(s: String): Option[Int] = {
     try {
       Some(s.toInt)
@@ -77,7 +65,6 @@ object SimpleSpark {
     }
   }
 
-  //--------------------------------------------------------------------------------------------
   // Aligns the dataset to the appropriate time period
   def trimToRegion(history: Array[(String, DateTime, Double)], start: DateTime, end: DateTime)
   : Array[(String, DateTime, Double)] = {
@@ -92,9 +79,6 @@ object SimpleSpark {
     trimmed
   }
 
-
-
-  //--------------------------------------------------------------------------------------------
   def dataMatrix(histories: Array[(String, Double)],  stockNameMap : Map[String,Double]): Array[Double] = {
     val mat = new Array[Double](histories.length)
     //test = test + 1
@@ -109,8 +93,6 @@ object SimpleSpark {
     mat :+ stockNameMap.get(histories(0)._1).get
   }
 
-
-  //--------------------------------------------------------------------------------------------
   // Fills missing data with values from nearest neighbor’s
   def fillInHistory(history: Array[(String, DateTime, Double)], start: DateTime, end: DateTime)
   : Array[(String, DateTime, Double)] = {
@@ -121,24 +103,17 @@ object SimpleSpark {
       if (cur.tail.nonEmpty && cur.tail.head._2 == curDate) {
         cur = cur.tail
       }
-
       filled += ((cur.head._1, curDate, cur.head._3))
       val a = 2.months
-
       curDate += 1.days
       // Skip weekends
       if (curDate.dayOfWeek().get > 5) curDate += 2.days
     }
     filled.toArray
   }
-
-
-
-
-  //--------------------------------------------------------------------------------------------
-  /**
-    * Reads a Stock history from each file
-    */
+  /*
+   * Reads a Stock history from each file
+   */
   def readStockHistory(file: File): Array[(String, DateTime, Double)] = {
     val format = new SimpleDateFormat("yyyy-MM-dd")
     val lines = Source.fromFile(file).getLines().toSeq
@@ -150,8 +125,7 @@ object SimpleSpark {
       (name(0), date, value)
     }).reverse.toArray
   }
-
-  /**
+  /*
     * Reads a Factor history from each file
     */
   def readFactorHistory(file: File): Array[(DateTime, Double)] = {
@@ -164,7 +138,6 @@ object SimpleSpark {
       (date, value)
     }).reverse.toArray
   }
-
   /**
     * Reads a all the Stock histories
     */
@@ -179,7 +152,6 @@ object SimpleSpark {
     })
   }
 
-  //--------------------------------------------------------------------------------------------
   def valueReturns(history: Array[(String, DateTime, Double)]): Array[(String, Double)] = {
     history.sliding(10).map { window =>
       val next = window.last._3
@@ -187,8 +159,6 @@ object SimpleSpark {
       (window.head._1, ((next - prev) / prev))
     }.toArray
   }
-
-
   //--------------------------------------------------------------------------------------------
   def factorMatrix(histories: Seq[Array[Double]]): Array[Array[Double]] = {
     val mat = new Array[Array[Double]](histories.head.length)
@@ -197,7 +167,6 @@ object SimpleSpark {
     }
     mat
   }
-
   //-----------------
   def dataModel(instrument: Array[Double], factorMatrix: Array[Array[Double]])= {
     val mat = new Array[Array[Double]](instrument.length-1)
@@ -209,241 +178,128 @@ object SimpleSpark {
     }
     mat
   }
-
   //--------------------
   def featurize(factorReturns: Array[Double]): Array[Double] = {
     val squaredReturns = factorReturns.map(x => math.signum(x) * x * x)
     val squareRootedReturns = factorReturns.map(x => math.signum(x) * math.sqrt(math.abs(x)))
     squaredReturns ++ squareRootedReturns ++ factorReturns
   }
-
   //---------------------------
   def computefactorWeights(dataset:Array[Array[Double]], algoNAme:String, sc:SparkContext)= {
-
-
     val stockID = dataset(0)(dataset(0).length-1)
-
     val data2 = dataset.map{ line =>
       LabeledPoint(line(line.length-2), Vectors.dense(line.dropRight(2)))
     }
-
     val data2RDD = sc.parallelize(data2)
-
-    // ALGORITHMS
-
-
     if (algoNAme == "LinearRegressionWithSGD_L0"){
-
-      // LR default L0
       var lrAlg = new LinearRegressionWithSGD()
       lrAlg.optimizer.setNumIterations(100).setStepSize(0.001)
       lrAlg.setIntercept(true)
-
-
       val model = lrAlg.run(data2RDD)
-
       var factorWeights = new Array[Double](model.weights.toArray.length)
-
       var i = 0
       for (wts <- model.weights.toArray){
-
         if (i < model.weights.toArray.length){
           factorWeights(i) = wts
           i = i + 1
-          //     println(wts)
-          //    println("#######################")
         }
-
-
       }
-
       println(model.intercept)
-
       factorWeights = factorWeights:+model.intercept
-
       factorWeights:+stockID
-
     }
-
     if (algoNAme == "LinearRegressionWithSGD_L1"){
-
-      // LR default L1
       var  lrAlg = new LinearRegressionWithSGD()
       lrAlg.optimizer.setNumIterations(100).setUpdater(new L1Updater).setStepSize(0.001)
       lrAlg.setIntercept(true)
-
       val model = lrAlg.run(data2RDD)
-
       var factorWeights = new Array[Double](model.weights.toArray.length)
-
       var i = 0
       for (wts <- model.weights.toArray){
-
         if (i < model.weights.toArray.length){
           factorWeights(i) = wts
           i = i + 1
-          //println(wts)
-          //println("#######################")
         }
-
-
       }
-
       factorWeights = factorWeights:+model.intercept
-
-
-
       factorWeights:+stockID
     }
-
     if (algoNAme == "LinearRegressionWithSGD_L2"){
-      // LR default L2
       var lrAlg = new LinearRegressionWithSGD()
       lrAlg.optimizer.setNumIterations(100).setUpdater(new SquaredL2Updater).setStepSize(0.001)
       lrAlg.setIntercept(true)
-
       val model = lrAlg.run(data2RDD)
-
       var factorWeights = new Array[Double](model.weights.toArray.length)
-
       var i = 0
       for (wts <- model.weights.toArray){
-
         if (i < model.weights.toArray.length){
           factorWeights(i) = wts
           i = i + 1
-          //println(wts)
-          //println("#######################")
         }
-
-
       }
-
       factorWeights = factorWeights:+model.intercept
-
-
-
       factorWeights:+stockID
     }
-
     if (algoNAme == "RidgeRegressionWithSGD"){
-      // RidgeRegressionWithSGD default L2
       var lrAlg = new RidgeRegressionWithSGD()
       lrAlg.optimizer.setNumIterations(100).setStepSize(0.001)
       lrAlg.setIntercept(true)
       val model = lrAlg.run(data2RDD)
-
       var factorWeights = new Array[Double](model.weights.toArray.length)
-
       var i = 0
       for (wts <- model.weights.toArray){
-
         if (i < model.weights.toArray.length){
           factorWeights(i) = wts
           i = i + 1
-          //println(wts)
-          //println("#######################")
         }
-
-
       }
-
       factorWeights = factorWeights:+model.intercept
-
-
-
       factorWeights:+stockID
     }
-
-
     if (algoNAme == "LassoWithSGD"){
       // LR default L2
       var  lrAlg = new LassoWithSGD()
       lrAlg.optimizer.setNumIterations(100).setStepSize(0.001)
       lrAlg.setIntercept(true)
-
       val model = lrAlg.run(data2RDD)
-
       var factorWeights = new Array[Double](model.weights.toArray.length)
-
       var i = 0
       for (wts <- model.weights.toArray){
-
         if (i < model.weights.toArray.length){
           factorWeights(i) = wts
           i = i + 1
-          //println(wts)
-          //println("#######################")
         }
-
-
       }
-
       factorWeights = factorWeights:+model.intercept
-
-
-
       factorWeights:+stockID
     }else{
-
     }
-
-
-
-
   }
-
   //-------------------------
   def LinearRegressionWithSGD_L0(dataset:Array[Array[Double]], sc:SparkContext)= {
-
-
     val stockID = dataset(0)(dataset(0).length-1)
-
     val data2 = dataset.map{ line =>
       LabeledPoint(line(line.length-2), Vectors.dense(line.dropRight(2)))
     }
-
     val data2RDD = sc.parallelize(data2)
-
-
-
-    // LR default L0
     var lrAlg = new LinearRegressionWithSGD()
     lrAlg.optimizer.setNumIterations(100).setStepSize(0.001)
     lrAlg.setIntercept(true)
-
-
     val model = lrAlg.run(data2RDD)
-
     var factorWeights = new Array[Double](model.weights.toArray.length)
-
     var i = 0
     for (wts <- model.weights.toArray){
-
       if (i < model.weights.toArray.length){
         factorWeights(i) = wts
         i = i + 1
-        //     println(wts)
-        //    println("#######################")
       }
-
-
     }
-
-    // println(model.intercept)
-
     factorWeights = factorWeights:+model.intercept
-
     factorWeights:+stockID
-
-
-
   }
-
-  //-----------------------------
-  /**
-    * Calculate the return of a particular instrument under particular trial conditions.
-    */
+  /*
+   * Calculate the return of a particular instrument under particular trial conditions.
+   */
   def instrumentTrialReturn(instrument: Array[Double], trial: Array[Double]): Double = {
     var instrumentTrialReturn = instrument(0)
     var i = 0
@@ -453,10 +309,9 @@ object SimpleSpark {
     }
     instrumentTrialReturn
   }
-
-  /**
-    * Calculate the full return of the portfolio under particular trial conditions.
-    */
+  /*
+   * Calculate the full return of the portfolio under particular trial conditions.
+   */
   def trialReturn(trial: Array[Double], instruments: Seq[Array[Double]]) = {
     var totalReturn = 0.0
     // var totalR = new Array[(Double,Array[(Double,Double)])] (1)
@@ -469,13 +324,9 @@ object SimpleSpark {
         individualTrialReturns(i) = (instrument(instrument.length-1),instrumentReturn)
         i= i+1
       }
-
     }
     ((totalReturn / instruments.size),individualTrialReturns)
-
   }
-
-  //-----------------------------
   def trialReturns(
                     seed: Long,
                     numTrials: Int,
@@ -485,24 +336,17 @@ object SimpleSpark {
     val rand = new MersenneTwister(seed)
     val multivariateNormal = new MultivariateNormalDistribution(rand, factorMeans,
       factorCovariances)
-
     val trialReturns = new Array[(Double,Array[(Double,Double)])](numTrials)
-
     for (i <- 0 until numTrials) {
-
       val trialFactorReturns = multivariateNormal.sample()
       val trialFeatures = featurize(trialFactorReturns)
       trialReturns(i) = trialReturn(trialFeatures, instruments)
     }
-
     trialReturns
-
   }
-
   //------------------------------
   def PercentVaR(trials: RDD[Double]): Double = {
     val percentParam = 100 / 10 //financialRiskAnalysisParam.getOrElse(0)
-
     val topLosses = trials.takeOrdered(math.max(trials.count().toInt / percentParam, 1))
     topLosses.last
   }
@@ -537,10 +381,7 @@ object SimpleSpark {
     }
     failures
   }
-
-
-  //------------------------------
-
+  
   def kupiecTestStatistic(total: Int, failures: Int, confidenceLevel: Double): Double = {
     val failureRatio = failures.toDouble / total
     val logNumer = (total - failures) * math.log1p(-confidenceLevel) +
@@ -560,194 +401,92 @@ object SimpleSpark {
     1 - new ChiSquaredDistribution(1.0).cumulativeProbability(testStatistic)
   }
 
-  //---------------------------
-
-
-
-
-
-
-
-
-
-
-  //------------------
-
   def main(args: Array[String]) {
-
-    //10 100 10 LassoWithSGD 100 1001L
     args.foreach(arg => println("--------------" + arg))
-
     val conf = new SparkConf().setMaster("local[2]").setAppName("Simple Application")
     val sc = new SparkContext(conf)
-
-    //--------------------------------------------------------------------------------------------
-    //Variables
-    val financialRiskAnalysisParam = args(0) //10 //toInt(z.input("Financial RiskAnalysis Param in %").toString)
-    val nTrials = args(1) //100 //toInt(z.input("Number Of Trials (Min 100)").toString)
-    val slidingWindow = args(2) //10 //toInt(z.input("Time interval").toString)
-
-    val algoNAme = args(3)// "LassoWithSGD" //z.input("Regression Algorithm To be used  (Values : LinearRegressionWithSGD_L0,LinearRegressionWithSGD_L1,LinearRegressionWithSGD_L2,RidgeRegressionWithSGD,LassoWithSGD)").toString
-
-    //-------------------------------------------------------------------------------------------
+    val financialRiskAnalysisParam = args(0) 
+    val nTrials = args(1) 
+    val slidingWindow = args(2) 
+    val algoNAme = args(3)    
     val filePrefix = "/home/smit/Documents/ScalaProject/FilterData/MapData/StockNamesMap.csv"
     val stockNameMap = readStockNameMap(new File(filePrefix))
-
-    //--------------------------------------------------------------------------------------------
-    // Declare Time range
     val start = new DateTime(2000, 2, 13, 0, 0)
     val end = new DateTime(2015, 8, 7, 0, 0)
-
     val rawStocks = readAllStockHistories(new File("/home/smit/Documents/ScalaProject/FilterData/stocks/")).filter(_.size >= 260 * 5 + 10)
-
     val rawStocksrdd = sc.parallelize(rawStocks)
     rawStocks.length
-
     val stocks = rawStocksrdd.map(trimToRegion(_, start, end)).map(fillInHistory(_, start, end))
     stocks.count
-
-    //--------------------------------------------------------------------------------------------
     val stocksReturns = stocks.map(valueReturns)
     stocksReturns.collect
-
-
-    //--------------------------------------------------------------------------------------------
-    //Load Factors
     val factorsPrefix = "/home/smit/Documents/ScalaProject/FilterData/factors/"
     val factors1 = Array("NDX.csv", "SNP.csv", "ShanghiaCompositeIndex.csv").
       map(x => new File(factorsPrefix + x)).
       map(readStockHistory)
-
     val fctors = factors1.
       map(trimToRegion(_, start, end)).
       map(fillInHistory(_, start, end))
-
     val factors = sc.parallelize(fctors)
     val factorsReturns = factors.map(valueReturns)
-
-
-    //--------------------------------------------------------------------------------------------
     val temp1 = factorsReturns.flatMap { arrayElement =>
       arrayElement filter {
         case (x: String, y: Double) => x == "NDX"
       }
     }
-
-
     val temp2 = factorsReturns.flatMap { arrayElement =>
       arrayElement filter {
         case (x: String, y: Double) => x == "SNP"
       }
     }
-
-//    val temp3 = factorsReturns.flatMap { arrayElement =>
-//      arrayElement filter {
-//        case (x: String, y: Double) => x == "ShanghiaCompositeIndex"
-//      }
-//    }
-
-
     val factor1 = temp1.map(x => x._2).toArray
     val factor2 = temp2.map(x => x._2).toArray
-    //val factor3 = temp3.map(x => x._2).toArray
-
     temp2.collect.foreach(println)
-
     val factorMat = factorMatrix(Seq(factor1, factor2))
-
     val factorMatrdd = sc.parallelize(factorMat)
-
-    //--------------------------------------------------------------------------------------------
     val factorFeatures = factorMatrdd.map(featurize)
     factorFeatures.collect
     factorFeatures.count
-
-    //--------------------------------------------------------------------------------------------
     var test = 0
-
-    //--------------------------------------------------------------------------------------------
     val spantemp = stocksReturns.toArray
-
     val labels = spantemp.map(i => dataMatrix(i,stockNameMap))
-
     val tblah = labels.length
-
     var rnd = new scala.util.Random
-
     val temp = labels.map(x => dataModel(x, factorFeatures.toArray))
-
     val t = temp.length
-
     val factorWts =  temp.map(i => LinearRegressionWithSGD_L0(i, sc))
-
-    // Covariance Calcultaion
-
     val factorCov = new Covariance(factorMat).getCovarianceMatrix().getData()
-
     val factorMeans = Seq(factor1,factor2).map(factor => factor.sum /factor.size).toArray
-
-
-
-    //factorFeatures
-
-
     val broadCastFactorWts = sc.broadcast(factorWts)
-
-    val numTrials = args(4).toInt //100 //nTrials.getOrElse(0)
+    val numTrials = args(4).toInt 
     val parallelism = numTrials / 10
-    val baseSeed = 1001L //args(5).toLong //1001L
-
-
-    // Generate different seeds so that our simulations don't all end up with the same results
+    val baseSeed = 1001L 
     baseSeed until baseSeed + parallelism
     val seeds = (baseSeed until baseSeed + parallelism)
     val seedRdd = sc.parallelize(seeds, parallelism)
-
-    // create an empty map
     var stocksTrialsReturns = scala.collection.mutable.Map[String, Double]()
-
-
     val trialsrdd = seedRdd.flatMap(
       trialReturns(_, numTrials / parallelism, broadCastFactorWts.value, factorMeans, factorCov))
-
     val trials = trialsrdd.map(line => line._1)
     trials.collect
-
-
     val stockreturns = trialsrdd.map(line => line._2)
-
     var j = scala.collection.mutable.Map[Double, Double]()
     val xyz = stockreturns.collect.flatten
-
-
     val stocksSummations = xyz.foreach(i => if(j.get(i._1) == None){
       j+= (i._1->i._2)}
     else {
       j+= (i._1 -> (j.get(i._1).get+ i._2))
     })
-
-
-
     val valueAtRisk = PercentVaR(trials)
     val ExpectedShortFall = PercentES(trials)
     println("VaR : " + valueAtRisk)
     println("ES : " + ExpectedShortFall)
-
-
-    // maybe you want to write it out as csv
     val csvResults = j map { case (key, value) => Array(key, value).mkString(",\t") }
-
-
-    //val myFilePrefix = "/home/hadoop/data/StockNamesMap.csv"
     val idTostockNameMap = readIdtoStockNameMap(new File(filePrefix))
-
-
-    //
     val varConfidenceInterval = confidenceInterval(trials, PercentVaR, 100, .05)
     val esConfidenceInterval = confidenceInterval(trials, PercentES, 100, .05)
     println("VaR confidence interval: " + varConfidenceInterval)
     println("ES confidence interval: " + esConfidenceInterval)
     println("Kupiec test p-value: " + BackTestPValue(labels.toSeq, valueAtRisk, 0.05))
-
   }
 }
